25f0a37d4615333d6241be1318d8918ba400940d
Michael Bestas <mkbestas@lineageos.org>
2019-01-07 22:27:39 +0200

    ulysse: Hex edit camera blobs to use /data/vendor/qcam
    (Partial commit)
    
    * Modify OSS HAL to use this path.
    
    for LIB in $(grep -lr "/data/misc/camera" .); do
        sed -i "s|/data/misc/camera|/data/vendor/qcam|g" "$LIB" || true
    done
    
    Change-Id: I220188d09b9da77a284ce40ac63d50c9054e8ba0

--- extract-files.sh
+++ extract-files.sh
@@ -23,6 +23,25 @@ function blob_fixup() {
         vendor/lib64/libvendor.goodix.hardware.fingerprint@1.0-service.so)
             "${PATCHELF_0_8}" --remove-needed "libprotobuf-cpp-lite.so" "${2}"
             ;;
+        vendor/lib/libmmcamera2_cpp_module.so \
+        |vendor/lib/libmmcamera2_dcrf.so \
+        |vendor/lib/libmmcamera2_iface_modules.so \
+        |vendor/lib/libmmcamera2_imglib_modules.so \
+        |vendor/lib/libmmcamera2_mct.so \
+        |vendor/lib/libmmcamera2_pproc_modules.so \
+        |vendor/lib/libmmcamera2_q3a_core.so \
+        |vendor/lib/libmmcamera2_sensor_modules.so \
+        |vendor/lib/libmmcamera2_stats_algorithm.so \
+        |vendor/lib/libmmcamera2_stats_modules.so \
+        |vendor/lib/libmmcamera_dbg.so \
+        |vendor/lib/libmmcamera_imglib.so \
+        |vendor/lib/libmmcamera_pdafcamif.so \
+        |vendor/lib/libmmcamera_pdaf.so \
+        |vendor/lib/libmmcamera_tintless_algo.so \
+        |vendor/lib/libmmcamera_tuning.so \
+        |vendor/lib/libmm-qcamera.so)
+            sed -i 's|data/misc/camera|data/vendor/qcam|g' "${2}"
+            ;;
     esac
 }
 
